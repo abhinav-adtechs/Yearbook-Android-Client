810e368dfa71b0b31f0cb7325ace9902f58fa202
Abhinav Das <abhinav.adtechs@gmail.com>
2017-10-01 00:34:49 +0530

    new fixed ui setup done

--- app/src/main/java/in/vit/yearbook/View/NewUI/Dashboard/DashboardFragment.java
+++ app/src/main/java/in/vit/yearbook/View/NewUI/Dashboard/DashboardFragment.java
@@ -1,20 +1,24 @@
 package in.vit.yearbook.View.NewUI.Dashboard;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
-import android.widget.FrameLayout;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-
-import com.airbnb.lottie.LottieAnimationView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,17 +28,24 @@ import in.vit.yearbook.View.NewUI.BaseFragment;
 
 public class DashboardFragment extends BaseFragment implements View.OnClickListener{
 
-    @BindView(R.id.new_fragment_dashboard_frame_download)
-    FrameLayout frameDownload ;
-
     @BindView(R.id.new_fragment_dashboard_iv_cover)
     ImageView ivCoverPhoto ;
 
     @BindView(R.id.new_fragment_dashboard_top_animation)
     RecyclerView rvDashboardTopBook;
 
-    DownloadPreviewFragment downloadPreviewFragment ;
-    //DownloadDashFragment downloadDashFragment ;
+    @BindView(R.id.new_fragment_dashboard_btn_download)
+    ImageButton ibDownload ;
+
+    @BindView(R.id.new_fragment_dashboard_tv_details)
+    TextView tvDownloadDetails ;
+
+    @BindView(R.id.new_fragment_dashboard_tv_size)
+    TextView tvDownloadSize ;
+
+    private boolean showStateSelected = false ;
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 101;
+
 
     @Nullable
     @Override
@@ -42,8 +53,8 @@ public class DashboardFragment extends BaseFragment implements View.OnClickListe
         View view = inflater.inflate(R.layout.new_fragment_dashboard, container, false) ;
         ButterKnife.bind(this, view);
 
-        frameDownload.setOnClickListener(this);
         ivCoverPhoto.setOnClickListener(this);
+        ibDownload.setOnClickListener(this);
 
         return view ;
     }
@@ -52,83 +63,104 @@ public class DashboardFragment extends BaseFragment implements View.OnClickListe
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        downloadPreviewFragment = new DownloadPreviewFragment() ;
-        addFragmentTransaction(downloadPreviewFragment);
-        //downloadDashFragment = new DownloadDashFragment() ;
-
     }
 
-
-    private void addFragmentTransaction(android.support.v4.app.Fragment currentFragment) {
-
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.new_fragment_dashboard_frame_download, currentFragment)
-                .commit();
-
-    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.new_fragment_dashboard_frame_download:
-
-                ScaleAnimation anim = new ScaleAnimation(1.0f, 2.0f, 1.0f, 2.0f, Animation.RELATIVE_TO_SELF,1.0f, Animation.RELATIVE_TO_SELF, 0.5f);
-                anim.setFillEnabled(true);
-                anim.setFillAfter(true);
-                anim.setDuration(400);
-                frameDownload.startAnimation(anim);
-                frameDownload.setClickable(false);
-
-                ScaleAnimation anim2 = new ScaleAnimation(1.0f, 0.5f, 1.0f, 0.5f, Animation.RELATIVE_TO_SELF,0.1f, Animation.RELATIVE_TO_SELF, 0.5f);
-                anim2.setFillEnabled(true);
-                anim2.setFillAfter(true);
-                anim2.setDuration(400);
-                ivCoverPhoto.startAnimation(anim2);
-                ivCoverPhoto.setClickable(true);
-
-                downloadPreviewFragment.increaseSize();
-
-                //makeFragmentTransaction(downloadDashFragment);
-                frameDownload.requestLayout();
-
-                break;
-
             case R.id.new_fragment_dashboard_iv_cover:
 
-                ScaleAnimation anim3 = new ScaleAnimation(2.0f, 1.0f, 2.0f, 1.0f, Animation.RELATIVE_TO_SELF,1.0f, Animation.RELATIVE_TO_SELF, 0.5f);
-                anim3.setFillEnabled(true);
-                anim3.setFillAfter(true);
-                anim3.setDuration(400);
-                frameDownload.startAnimation(anim3);
-                frameDownload.setClickable(true);
+                if (!showStateSelected){
+                    ivCoverPhoto.startAnimation(slideLeftAnimation());
+                    AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f) ;
+                    fadeIn.setDuration(1200);
+                    fadeIn.setFillEnabled(true);
+                    fadeIn.setFillAfter(true);
+                    tvDownloadDetails.setAnimation(fadeIn);
+                    tvDownloadSize.setAnimation(fadeIn);
+                    ibDownload.setAnimation(fadeIn);
+                    ibDownload.setVisibility(View.VISIBLE);
+                    showStateSelected = true ;
+                    ibDownload.setEnabled(true);
+                }else {
+                    ivCoverPhoto.startAnimation(slideRightAnimation());
+                    AlphaAnimation fadeOut= new AlphaAnimation(1.0f, 0.0f) ;
+                    fadeOut.setDuration(800);
+                    fadeOut.setFillEnabled(true);
+                    fadeOut.setFillAfter(true);
+                    tvDownloadDetails.setAnimation(fadeOut);
+                    tvDownloadSize.setAnimation(fadeOut);
+                    ibDownload.setAnimation(fadeOut);
+                    ibDownload.setVisibility(View.VISIBLE);
+                    showStateSelected = false ;
+                    ibDownload.setEnabled(false);
+                }
 
-                ScaleAnimation anim4 = new ScaleAnimation(0.5f, 1.0f, 0.5f, 1.0f, Animation.RELATIVE_TO_SELF,0.1f, Animation.RELATIVE_TO_SELF, 0.5f);
-                anim4.setFillEnabled(true);
-                anim4.setFillAfter(true);
-                anim4.setDuration(400);
-                ivCoverPhoto.startAnimation(anim4);
-                ivCoverPhoto.setClickable(false);
+                break;
 
-                downloadPreviewFragment.decreaseSize();
-
-                //makeFragmentTransaction(downloadPreviewFragment);
-                frameDownload.requestLayout();
+            case R.id.new_fragment_dashboard_btn_download:
+                Log.i("TAG", "onClick: ");
+                if (ActivityCompat.checkSelfPermission(
+                        getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+                }else {
+                    startDownloading() ;
+                }
                 break;
 
         }
     }
 
-    private void makeFragmentTransaction(android.support.v4.app.Fragment nextFragment) {
+    private Animation slideLeftAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, -0.45f,
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f);
+        inFromLeft.setDuration(500);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        inFromLeft.setFillEnabled(true);
+        inFromLeft.setFillAfter(true);
+        return inFromLeft;
+    }
 
-        if (nextFragment != null){
-            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .setCustomAnimations(R.anim.fade_in_slow, R.anim.fade_out_slow)
-                    .replace(R.id.new_fragment_dashboard_frame_download, nextFragment)
-                    .commit();
+    private Animation slideRightAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, -0.45f,
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f);
+        inFromLeft.setDuration(500);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        inFromLeft.setFillEnabled(true);
+        inFromLeft.setFillAfter(true);
+        return inFromLeft;
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT){
+            Log.i("TAG", "onRequestPermissionsResult: " + grantResults[0]);
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT){
+            Log.i("TAG", "onActivityResult: ");
+        }
+    }
+
+    private void startDownloading() {
+
+
 
     }
+
 }
--- app/src/main/java/in/vit/yearbook/View/NewUI/Dashboard/DownloadPreviewFragment.java
+++ app/src/main/java/in/vit/yearbook/View/NewUI/Dashboard/DownloadPreviewFragment.java
@@ -1,30 +1,42 @@
 package in.vit.yearbook.View.NewUI.Dashboard;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import in.vit.yearbook.R;
 import in.vit.yearbook.View.NewUI.BaseFragment;
+import in.vit.yearbook.View.NewUI.MainActivity;
 
-public class DownloadPreviewFragment extends BaseFragment{
+public class DownloadPreviewFragment extends BaseFragment implements View.OnClickListener{
 
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 101;
     @BindView(R.id.new_fragment_downloading_prev_tv_size)
     TextView tvSize ;
+
+    @BindView(R.id.new_fragment_downloading_prev_btn_downloading)
+    ImageView ivDownload ;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_fragment_downloading_preview, container, false) ;
         ButterKnife.bind(this, view) ;
-
+        Log.i("TAG", "onCreateView: ");
+        ivDownload.setOnClickListener(this);
         return view ;
     }
 
@@ -39,6 +51,41 @@ public class DownloadPreviewFragment extends BaseFragment{
     }
 
     public void decreaseSize(){
+        tvSize.setVisibility(View.INVISIBLE);
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.new_fragment_downloading_prev_btn_downloading){
+
+            Log.i("TAG", "Downloading begin ");
+
+            if (ActivityCompat.checkSelfPermission(
+                    getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+            }
+
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT){
+            Log.i("TAG", "onRequestPermissionsResult: ");
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT){
+            Log.i("TAG", "onActivityResult: ");
+        }
     }
 }
