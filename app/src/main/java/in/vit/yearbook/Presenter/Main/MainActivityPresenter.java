392d129be99e947d15013ed0e9f44c6075e15658
Abhinav Das <abhinav.adtechs@gmail.com>
2017-10-16 23:19:18 +0530

    RAM 'buddy fix'

--- app/src/main/java/in/vit/yearbook/Presenter/Main/MainActivityPresenter.java
+++ app/src/main/java/in/vit/yearbook/Presenter/Main/MainActivityPresenter.java
@@ -2,9 +2,6 @@ package in.vit.yearbook.Presenter.Main;
 
 
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
