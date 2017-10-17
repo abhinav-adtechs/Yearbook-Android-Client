3e48161cf47d7a4b7d0352f25d62a8ce9c9dcb25
Abhinav Das <abhinav.adtechs@gmail.com>
2017-10-18 00:41:30 +0530

    Following changes in the resulting code - fixes for all screen sizes - firebase integration - crashlytics integration

--- app/src/main/java/in/vit/yearbook/AppController.java
+++ app/src/main/java/in/vit/yearbook/AppController.java
@@ -2,10 +2,12 @@ package in.vit.yearbook;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import in.vit.yearbook.Dagger.Components.AppComponent;
 import in.vit.yearbook.Dagger.Components.DaggerAppComponent;
 import in.vit.yearbook.Dagger.Modules.SharedPrefModule;
 import in.vit.yearbook.Model.Utils.FontsOverride;
+import io.fabric.sdk.android.Fabric;
 
 
 public class AppController extends Application {
@@ -15,6 +17,7 @@ public class AppController extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         this.appComponent = DaggerAppComponent.builder()
                 .sharedPrefModule(new SharedPrefModule(this))
